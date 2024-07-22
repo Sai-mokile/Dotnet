003d7e4ad2f0de05ff6b77fba10167230a169354
sai-mokile <saismart2670@gmail.com>
2024-07-22 18:00:40 +0530

    log-3

--- My-app/ClientApp/src/components/Account/LogIn.js
+++ My-app/ClientApp/src/components/Account/LogIn.js
@@ -41,7 +41,7 @@ const LogIn = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     let formData = new FormData();
-    formData.append("Email", Email);
+    formData.append("Email", emailOrPhone);
     formData.append("password", password);
 
     fetch("api/account/login", {
